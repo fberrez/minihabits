d739a951466dfe9ec70ab0a79cfa80154f1c6560
fberrez <github.alphabet035@passinbox.com>
2025-01-10 14:26:16 +0100

    feat(api): enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure CORS
+  app.enableCors({
+    origin: ['https://minihabits-web-production.up.railway.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
