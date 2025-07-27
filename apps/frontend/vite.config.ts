e61b16e58d665196ee0e9988acb7c5cd1ffb2e39
fberrez <github.alphabet035@passinbox.com>
2025-07-27 16:53:14 +0200

    feat(tauri): Integrate Tauri for ios application support
    - Added Tauri configuration files and setup for building a desktop application.
    - Introduced new Rust files for Tauri application entry points.
    - Updated package.json to include Tauri CLI as a development dependency.
    - Removed unused assets and added new icons for the application.
    - Configured build settings and environment variables for Tauri integration.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,68 +3,43 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 // import { VitePWA } from 'vite-plugin-pwa';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
-  plugins: [
-    react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   injectRegister: 'auto',
-    //   strategies: 'generateSW',
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,jsx,css,html,png,svg,json,webp,mp3}'],
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: /^https:\/\/minihabits-production\.up\.railway\.app/g,
-    //         handler: 'NetworkFirst',
-    //         options: {
-    //           cacheName: 'api-cache',
-    //           expiration: {
-    //             maxEntries: 50,
-    //             maxAgeSeconds: 5 * 60, // 5 minutes
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    //   includeAssets: [
-    //     'favicon.ico',
-    //     'robots.txt',
-    //     'h-512.png',
-    //     'h-192.png',
-    //     'success.mp3',
-    //   ],
-    //   manifest: {
-    //     name: 'MiniHabits',
-    //     description: 'A simple habit tracker',
-    //     short_name: 'MiniHabits',
-    //     theme_color: '#ffffff',
-    //     background_color: '#ffffff',
-    //     display: 'standalone',
-    //     start_url: '/',
-    //     icons: [
-    //       {
-    //         src: 'h-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/webp',
-    //       },
-    //       {
-    //         src: 'h-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    // make sure this port matches the devUrl port in tauri.conf.json file
+    port: 5173,
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+
+    watch: {
+      // tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
   build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+
     assetsInlineLimit: 0, // Disable inlining assets to ensure MP3 files are properly copied
     rollupOptions: {
       output: {
